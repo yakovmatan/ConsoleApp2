fa17c6d3169843a80c56030a2089b60b4848ee33
yakov matan <yakovmatan@gmail.com>
2025-06-08 12:17:30 +0300

    create Agents class

---
+++ Models/Agents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Models
+{
+    internal class Agents
+    {
+        private int id;
+        private string codeName;
+        private string realName;
+        private string location;
+        private string status;
+        private int missiionCompleted;
+
+        public Agents(int id, string codeName, string realName, string location, string status, int missiionCompleted)
+        {
+            this.id = id;
+            this.codeName = codeName;
+            this.realName = realName;
+            this.location = location;
+            this.status = status;
+            this.missiionCompleted = missiionCompleted;
+        }
+    }
+}
